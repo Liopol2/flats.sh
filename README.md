cf091efd3c5d67a9b268b6a2d605c7c0121ee6b2
Liopol2 <leonardosoumas3@gmail.com>
2024-06-27 15:26:19 -0300

    new README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Flats.sh 
+A simple bash script that reads and runs the txt file line per line.
+## Dependencies
+- [Flatpak](https://flatpak.org/setup/) 
+That's it.
+## Usage
+1. Find the packages you want on [Flathub](https://flathub.org/) 
+2. Paste the command from flathub to the txt file.
+![flathubexample](example.png)
+   <div align="center">In this case `flatpak install flathub md.obsidian.Obsidian`</div> 
+3. Then run the script as you wish. eg `. flat.sh`
--- flat.sh
+++ flat.sh
@@ -2,12 +2,26 @@
 
 # Loop through each line in the file
 while read -r line; do
-  # Extract the last part of the line after the second . character
-  app=$(echo "$line" | awk -F'[.]' '{print $(NF)}')
 
+  IFS=' ' read -ra command <<< "$line"
+
+  # Count word position
+  i=1
+  for word in "${command[@]}"; do
+    if [ $i -eq 3 ]; then
+      break
+    fi
+    ((i++))
+  done
+
+  # Change the command to auto-confirm
+  self_confirming_command="${command[@]:0:2} -y ${command[@]:2:3}"
+  #echo "New command: $self_confirming_command"
+
+  app="${command[@]:3}"
   # Output the app name
   echo "Installing $app"
 
   # Run the command from the line
-  eval "$line"
+  eval "$self_confirming_command"
 done < flatpacks.txt
