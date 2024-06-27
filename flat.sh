32204ed2305be5657ab94b862c2255a393c4e602
Liopol2 <60528398+Liopol2@users.noreply.github.com>
2024-06-27 16:09:23 -0300

    Update flat.sh
    Added a few more comments.

--- flat.sh
+++ flat.sh
@@ -2,7 +2,7 @@
 
 # Loop through each line in the file
 while read -r line; do
-
+  # Separate the line at the " " and assign it to the command variable as an array
   IFS=' ' read -ra command <<< "$line"
 
   # Count word position
@@ -17,11 +17,14 @@ while read -r line; do
   # Change the command to auto-confirm
   self_confirming_command="${command[@]:0:2} -y ${command[@]:2:3}"
   #echo "New command: $self_confirming_command"
-
+  
+  # Get app name 
   app="${command[@]:3}"
+  
   # Output the app name
   echo "Installing $app"
 
   # Run the command from the line
   eval "$self_confirming_command"
+#Redirect flatpack.txt to the loop
 done < flatpacks.txt
